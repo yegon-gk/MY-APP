fd6374499bc27cd25e5cfa0cf22852b95d0ab373
yegon-gk <kiprotych@gmail.com>
2024-05-10 11:32:53 +0300

    I modified this Files.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,11 @@
+import Main from './components/Main';
 import Sidenav from './components/Sidenav';
 
 function App() {
   return (
     <div className="">
       <Sidenav />
+      <Main />
     </div>
   );
 }
