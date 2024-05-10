fac2b4656b6a9af64fc6a815d786fa2d55b512c5
yegon-gk <kiprotych@gmail.com>
2024-05-10 03:27:49 +0300

    I create Side bar and Added its toggle bar component.

--- client/src/components/Sidenav.jsx
+++ client/src/components/Sidenav.jsx
@@ -1,9 +1,103 @@
-import React from 'react'
+import React from 'react';
+import { useState } from 'react';
+import {
+  AiOutlineHome,
+  AiOutlineMenu,
+  AiOutlineProject,
+  AiOutlineMail,
+} from 'react-icons/ai';
+import { GrProjects } from 'react-icons/gr';
+import { BsPerson } from 'react-icons/bs';
 
 const Sidenav = () => {
+  const [nav, setNav] = useState(false);
+  const handleNav = () => {
+    setNav(!nav);
+    console.log('state changed!!');
+  };
   return (
-    <div>Sidenav</div>
-  )
-}
+    <div>
+      <AiOutlineMenu
+        onClick={handleNav}
+        className="absolute top-4 right-4 z-[99] md:hidden"
+      />
+      {nav ? (
+        <div className="fixed w-full h-screen bg-white/90 flex flex-col justify-center items-center z-20">
+          <a
+            href="#main"
+            className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-2 cursor-pointer hover:scale-110 ease-in duration-200  hover:bg-orange-500"
+          >
+            <AiOutlineHome size={20} />
+            <span className="pl-4">Home</span>
+          </a>
+          <a
+            href="#work"
+            className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-2 cursor-pointer hover:scale-110 ease-in duration-200  hover:bg-orange-500"
+          >
+            <GrProjects size={20} />
+            <span className="pl-4">Work</span>
+          </a>
+          <a
+            href="#projects"
+            className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-2 cursor-pointer hover:scale-110 ease-in duration-200  hover:bg-orange-500"
+          >
+            <AiOutlineProject size={20} />
+            <span className="pl-4">Projects</span>
+          </a>
+          <a
+            href="#main"
+            className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-2 cursor-pointer hover:scale-110 ease-in duration-200  hover:bg-orange-500"
+          >
+            <BsPerson size={20} />
+            <span className="pl-4">Resume</span>
+          </a>
+          <a
+            href="#contacts"
+            className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-2 cursor-pointer hover:scale-110 ease-in duration-200 hover:bg-orange-500"
+          >
+            <AiOutlineMail size={20} />
+            <span className="pl-4">Contacts</span>
+          </a>
+        </div>
+      ) : (
+        ''
+      )}
+      <div className="md:block hidden fixed top-[25%] z-10">
+        <div className="flex flex-col">
+          <a
+            href="#main"
+            className="rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300 hover:bg-orange-500"
+          >
+            <AiOutlineHome />
+          </a>
+          <a
+            href="#work"
+            className="rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300 hover:bg-orange-500"
+          >
+            <GrProjects />
+          </a>
+          <a
+            href="#projects"
+            className="rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300 hover:bg-orange-500"
+          >
+            <AiOutlineProject />
+          </a>
+          <a
+            href="#main"
+            className="rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300 hover:bg-orange-500"
+          >
+            <BsPerson />
+          </a>
+          <a
+            href="#contacts"
+            className="rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300 hover:bg-orange-500"
+          >
+            <AiOutlineMail />
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Sidenav
+export default Sidenav;
