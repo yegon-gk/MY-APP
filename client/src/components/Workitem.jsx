13c0ff8a94bb9d49fe0024424bddfc91c1217dc6
yegon-gk <kiprotych@gmail.com>
2024-05-10 19:15:09 +0300

    I updated the Work page UI

--- client/src/components/Workitem.jsx
+++ client/src/components/Workitem.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from 'react';
 
-const Workitem = () => {
+const Workitem = ({ year, title, duration, details }) => {
   return (
-    <div>Workitem</div>
-  )
-}
+    <ol className="flex flex-col md:flex-row relative border-l border-orange-500">
+      <li className="mb-10 ml-4">
+        <div className="absolute w-3 h-3 bg-stone-700 rounded-full mt-1.5 -left-1.5 border-white" />
+        <p className="flex flex-wrap gap-4 flex-row items-center justify-start text-xs md:text-sm">
+          <span className="inline-block px-2 py-1 font-semibold text-white bg-[#ff631b] rounded-md">
+            {year}
+          </span>
+          <span className="text-lg font-semibold text-[#ff631b]">{title}</span>
+          <span className="my-1 text-sm font-semibold leading-none text-stone-700 ">{duration}</span>
+          <span className="my-2 text-base font-normal leading-none text-slate-700">{details}</span>
+        </p>
+      </li>
+    </ol>
+  );
+};
 
-export default Workitem
+export default Workitem;
