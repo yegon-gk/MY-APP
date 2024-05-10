98135cd46261629aa7c83088230ef1bf423f8821
yegon-gk <kiprotych@gmail.com>
2024-05-10 16:25:34 +0300

    I created the Data for Works and its function.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,13 @@
 import Main from './components/Main';
 import Sidenav from './components/Sidenav';
+import Work from './components/Work';
 
 function App() {
   return (
     <div className="">
       <Sidenav />
       <Main />
+      <Work />
     </div>
   );
 }
---
+++ client/src/components/Work.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import WorkItem from './Workitem';
+
+const data = [
+  {
+    year: 2024,
+    title: 'Lead Full-Stack Developer.',
+    duration: '2 Years.',
+    details:
+      "I successfully developed and implemented a comprehensive social media integration feature for a client's website. This included incorporating icons with hover effects for various platforms, ensuring each icon links to the respective profiles or pages in new tabs. The implementation enhanced user engagement and connectivity.",
+  },
+  {
+    year: 2022,
+    title: 'Back-End Developer.',
+    duration: '3 Years.',
+    details:
+      "I successfully developed and implemented a comprehensive social media integration feature for a client's website. This included incorporating icons with hover effects for various platforms, ensuring each icon links to the respective profiles or pages in new tabs. The implementation enhanced user engagement and connectivity.",
+  },
+  {
+    year: 2020,
+    title: 'Software Engineer.',
+    duration: '3 Years.',
+    details:
+      "I successfully developed and implemented a comprehensive social media integration feature for a client's website. This included incorporating icons with hover effects for various platforms, ensuring each icon links to the respective profiles or pages in new tabs. The implementation enhanced user engagement and connectivity.",
+  },
+  {
+    year: 2019,
+    title: 'Junior Software Engineer.',
+    duration: '3 Years.',
+    details:
+      "I successfully developed and implemented a comprehensive social media integration feature for a client's website. This included incorporating icons with hover effects for various platforms, ensuring each icon links to the respective profiles or pages in new tabs. The implementation enhanced user engagement and connectivity.",
+  },
+];
+
+const Work = () => {
+  return (
+    <div id="work" className="max-w-[1040px] m-auto md:pl-20 p-4 py-16">
+      <h1 className="text-4xl font-bold text-center text-[#ff6f00]">
+        Work Experience
+      </h1>
+      {data.map((item, idx) => (
+        <WorkItem
+          key={idx}
+          year={item.year}
+          title={item.title}
+          duration={item.duration}
+          details={item.details}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default Work;
